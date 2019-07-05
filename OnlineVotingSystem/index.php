e84d0f8171728390c508c2c1402a35bb44279726
Sachin Roy <sroy895@gmail.com>
2019-07-05 17:42:49 +0530

    Create index.php
    Starting online voting system project

---
+++ OnlineVotingSystem/index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<title>SR | Voting System - Home</title>
+</head>
+<body>
+	<p><center><legend><font color='#008000' size='18'>This system allows all registered users to vote for their favorite 
+    <strong>PROGRAMMING LANGUAGE</strong>.</p>
+	<p>In order to make a vote you have to register first and then login.</font></legend></center>
+	<p>&nbsp;&nbsp;</p>
+</body>
+</html>
