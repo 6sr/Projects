f484f815441b52a43702f152803e929971f3fe62
Sachin Roy <sachinroy784@gmail.com>
2019-07-22 00:18:20 +0530

    Introducing templating engine

--- BlogNodeJS/index.js
+++ BlogNodeJS/index.js
@@ -1,27 +1,45 @@
 const path = require('path')
-
 const express = require('express')
+const expressEdge = require('express-edge')
+const mongoose = require('mongoose')
 
 const app = new express()
+
+mongoose.connect('mongodb://localhost/node-js-blog')
 
 app.use(express.static('pages'))
 //app.use(express.static(path.join(__dirname,'pages')))
 
+app.use(expressEdge)
+app.set('views',`${__dirname}/views`)
+
 
 app.get('/', (req,res) => {
-    res.sendfile(path.resolve(__dirname,'pages/index.html'))
+    //res.sendfile(path.resolve(__dirname,'pages/index.html'))
+    res.render('index')
+})
+
+app.get('/posts/new', (req,res) => {
+    res.render('create')
+})
+
+app.post('/posts/store', (req,res) => {
+    res.redirect('/')
 })
 
 app.get('/about', (req,res) => {
-    res.sendfile(path.resolve(__dirname,'pages/about.html'))
+    //res.sendfile(path.resolve(__dirname,'pages/about.html'))
+    res.render('about')
 })
 
 app.get('/post', (req,res) => {
-    res.sendfile(path.resolve(__dirname,'pages/post.html'))
+    //res.sendfile(path.resolve(__dirname,'pages/post.html'))
+    res.render('post')
 })
 
 app.get('/contact', (req,res) => {
-    res.sendfile(path.resolve(__dirname,'pages/contact.html'))
+    //res.sendfile(path.resolve(__dirname,'pages/contact.html'))
+    res.render('contact')
 })
 
 
